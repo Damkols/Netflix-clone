18d1d32f56a02b56faf0994de24ff649b735c8f4
Damkols <damolausmankolapo@gmail.com>
2023-03-27 13:03:55 +0100

    chore: loading component error

--- components/Modal/Modal.tsx
+++ components/Modal/Modal.tsx
@@ -12,17 +12,6 @@ import {
  XMarkIcon,
 } from "@heroicons/react/24/outline";
 import { FaVolumeUp, FaVolumeOff } from "react-icons/fa";
-// import {
-//  collection,
-//  deleteDoc,
-//  doc,
-//  DocumentData,
-//  onSnapshot,
-//  setDoc,
-// } from "firebase/firestore";
-// import { db } from "@/firebase";
-// import useAuth from "@/hooks/useAuth";
-// import toast, { Toaster } from 'react-hot-toast'
 
 const Modal = () => {
  const [showModal, setShowModal] = useRecoilState(modalState);
@@ -84,65 +73,22 @@ const Modal = () => {
       playing
       muted={muted}
      />
-     {/* <div className="absolute bottom-10 flex w-full items-center justify-between px-10">
-      <div className="flex space-x-2">
+     <div className="absolute bottom-10 flex w-full items-center justify-between px-10">
+      <div>
        <button className="flex items-center gap-x-2 rounded bg-white px-8 text-xl font-bold text-black transition hover:bg-[#e6e6e6]">
         <FaPlay className="h-7 w-7 text-black" />
-        Play
-       </button>
-       <button className="modalButton" onClick={handleList}>
-        {addedToList ? (
-         <CheckIcon className="h-7 w-7" />
-        ) : (
-         <PlusIcon className="h-7 w-7" />
-        )}
        </button>
        <button className="modalButton">
-        <HandThumbUpIcon className="h-6 w-6" />
+        <PlusIcon className="h-7 w-7" />
+       </button>
+
+       <button className="modalButton">
+        <HandThumbUpIcon className="h-7 w-7" />
        </button>
       </div>
-      <button className="modalButton" onClick={() => setMuted(!muted)}>
-       {muted ? (
-        <FaVolumeOff className="h-6 w-6" />
-       ) : (
-        <FaVolumeUp className="h-6 w-6" />
-       )}
-      </button>
-     </div> */}
-    </div>
-    {/* <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
-     <div className="space-y-6 text-lg">
-      <div className="flex items-center space-x-2 text-sm">
-       <p className="font-semibold text-green-400">
-        {movie!.vote_average * 10}% Match
-       </p>
-       <p className="font-light">
-        {movie?.release_date || movie?.first_air_date}
-       </p>
-       <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">
-        HD
-       </div>
-      </div>
-      <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
-       <p className="w-5/6">{movie?.overview}</p>
-       <div className="flex flex-col space-y-3 text-sm">
-        <div>
-         <span className="text-[gray]">Genres:</span>{" "}
-         {genres.map((genre) => genre.name).join(", ")}
-        </div>
-
-        <div>
-         <span className="text-[gray]">Original language:</span>{" "}
-         {movie?.original_language}
-        </div>
-
-        <div>
-         <span className="text-[gray]">Total votes:</span> {movie?.vote_count}
-        </div>
-       </div>
-      </div>
+      <button>{muted ? <FaVolumeOff /> : <FaVolumeUp />}</button>
      </div>
-    </div> */}
+    </div>
    </>
   </MuiModal>
  );
